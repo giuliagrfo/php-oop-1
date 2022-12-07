2ce2a4e98b611abeef3e0060b32640bc1bf9ee01
giuliagrfo <giuliagrifo1@gmail.com>
2022-12-07 15:17:04 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- 
+Definire una classe ‘Movie’
+- all'interno della classe sono dichiarate delle variabili d'istanza
+- all'interno della classe è definito un costruttore
+- all'interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
+
+<?php
+
+class Movie
+{
+}
