36a91166a81a58cb8a08c8e7c20bd9adf8bd2bc5
giuliagrfo <giuliagrifo1@gmail.com>
2022-12-07 17:00:10 +0100

    add method

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ class Movie
     public $genre;
     public $vote;
 
-    // - all'interno della classe è definito un costruttore
+    // all'interno della classe definire un costruttore
     function __construct($title, $duration, $genre, $vote)
     {
         $this->title = $title;
@@ -23,4 +23,33 @@ class Movie
         $this->genre = $genre;
         $this->vote = $vote;
     }
+
+    // all'interno della classe è definito almeno un metodo
+    public function getDuration($duration)
+    {
+        if ($duration > 150) {
+            $this->duration .= 'long movie';
+        } else {
+            $this->type = 'short film';
+        }
+    }
+
+    public function getDetails()
+    {
+        return "<br>" . "Title: " . $this->title . "<br/>" .
+            "Duration: " . $this->duration . "<br/>" .
+            "Genre: " . $this->genre . "<br/>" .
+            "Vote: " . $this->vote . "min" . "<br/>";
+    }
 }
+$ForrestGump = new Movie('Forrest Gump', 142, 'Dramatic comedy', 4, 4);
+$ForrestGump->getDuration($ForrestGump->duration);
+
+$Dunkirk = new Movie('Dunkirk', 106, 'Dramatic war history', 4, 1);
+$Dunkirk->getDuration($Dunkirk->duration);
+
+// var_dump($ForrestGump);
+// var_dump($Dunkirk);
+
+echo $ForrestGump->getDetails();
+echo $Dunkirk->getDetails();
